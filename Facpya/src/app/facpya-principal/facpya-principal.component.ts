bf18b273663fa80d5dfbc08cba990c24a5842070
EnriqueSKW <50007752+EnriqueSKW@users.noreply.github.com>
2021-04-28 12:35:26 -0500

    cambios
    se modifico el index, se migraron las funciones ya existentes a angular y se cambio el view del grid y se agrego boostrap

--- Facpya/src/app/facpya-principal/facpya-principal.component.ts
+++ Facpya/src/app/facpya-principal/facpya-principal.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./facpya-principal.component.css']
 })
 export class FacpyaPrincipalComponent implements OnInit {
-
+  private fieldArray: Array<any> = [];
+  private newAttribute: any = {};
   constructor() { }
-
+  public lista: any = 5;
   ngOnInit(): void {
+  }
+  counter(i: number) {
+    return new Array(i);
+}
+
+  addFieldValue() {
+    this.fieldArray.push(this.newAttribute)
+    this.newAttribute = {};
+  }
+
+  deleteFieldValue(index:any) {
+      this.fieldArray.splice(index, 1);
   }
 
 }

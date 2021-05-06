c4c27fbc1d5d9144f8f9603b10640bea4f70431f
EnriqueSKW <50007752+EnriqueSKW@users.noreply.github.com>
2021-05-06 16:43:26 -0500

    cambios finales
    se agregaron las funciones principales del costo promedio y se agrego la otra tabla funcional

--- Facpya/src/app/facpya-principal/facpya-principal.component.ts
+++ Facpya/src/app/facpya-principal/facpya-principal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit,ElementRef, ViewChild } from '@angular/core';
+import { Component, Inject, OnInit,ElementRef, ViewChild, Input } from '@angular/core';
 import { DOCUMENT } from '@angular/common'; 
 @Component({
   selector: 'app-facpya-principal',
@@ -10,7 +10,11 @@ export class FacpyaPrincipalComponent implements OnInit {
   private newAttribute: any = {};
   @ViewChild('container', { static: false }) public mydiv: ElementRef | undefined;
   public SumaSaldo:number = 0;
+  public SaldoArrastrado:number = 0;
+  public esEntrada:boolean = false;
+  public TablaPromedio:boolean = true;
   constructor() { }
+  @Input() CostoMedioLinea:number = 0;
   public lista: any = 5;
   ngOnInit(): void {
   }
@@ -20,7 +24,7 @@ export class FacpyaPrincipalComponent implements OnInit {
 
   public CalcularDebe(id:any)
   {
-    let Existencias:any = (<HTMLInputElement>document.getElementById("Existencia"+id)).value
+    let Existencias:any = (<HTMLInputElement>document.getElementById("Entrada"+id)).value
     let PrecioUnitario:any = (<HTMLInputElement>document.getElementById("Unitario"+id)).value;
     if (Existencias == null || Existencias == ""  )
     {
@@ -30,19 +34,67 @@ export class FacpyaPrincipalComponent implements OnInit {
     {
       PrecioUnitario = 0
     }
+  
+    
+    if( (<HTMLInputElement>document.getElementById("Debe"+id)).disabled == true)
+    {
+      var resultado:number = Existencias * PrecioUnitario;
+      resultado = Math.trunc(resultado);
+      (<HTMLInputElement>document.getElementById("Debe"+id)).value =  resultado.toString();
+      this.setSaldoSalida(id); 
+    }
+    else
+    { //es una entrada y calculamos como entrada  
     var resultado:number = Existencias * PrecioUnitario;
+    resultado = Math.trunc(resultado);
     (<HTMLInputElement>document.getElementById("Debe"+id)).value =  resultado.toString();
+    this.setSaldo(id,resultado); 
+    }
+ 
+  }
 
-   
-    this.setSaldo(id,resultado);
+  public setCostoMedio(id:any)
+  {
+  var existencia =  (<HTMLInputElement>document.getElementById("Existencia"+id)).value; 
+  var saldo =  (<HTMLInputElement>document.getElementById("Saldo"+id)).value;
+  (<HTMLInputElement>document.getElementById("Medio"+id)).value = (parseInt(saldo) / parseInt(existencia) ).toString();
   }
 
   public setSaldo(id:any,NuevaSuma:any)
   {
     this.SumaSaldo = 0;
+    let sumatotal:number = 0;
+    let i:number = 0;
+    console.log(id);
     this.SumaSaldo = this.SumaSaldo + NuevaSuma;
-    (<HTMLInputElement>document.getElementById("Saldo"+id)).value = this.SumaSaldo.toString() 
-    //hacer un ciclo con todos los id Debe  y sumarlos a la varibale global
+    if (id == 0)
+    {
+    (<HTMLInputElement>document.getElementById("Saldo"+id)).value = this.SumaSaldo.toString();
+    }
+    else
+    {
+      var identificador = id-1
+      console.log(identificador);
+      var saldoAnterior = (<HTMLInputElement>document.getElementById("Saldo"+identificador)).value;
+      this.SumaSaldo = this.SumaSaldo + parseInt(saldoAnterior);
+      console.log(this.SumaSaldo);
+      (<HTMLInputElement>document.getElementById("Saldo"+id)).value = this.SumaSaldo.toString();
+    }
+ 
+    this.setCostoMedio(id);
+  }
+
+  setSaldoSalida(id:any)
+  {
+   var Salida =  (<HTMLInputElement>document.getElementById("Salida"+id)).value;
+   console.log("salida =" + Salida);
+   var CostoMedioUnitario =  (<HTMLInputElement>document.getElementById("Unitario"+id)).value;
+   console.log("CostoMedioUnitario =" + CostoMedioUnitario);
+    var CostoSalida =  parseFloat(CostoMedioUnitario) * parseFloat(Salida);
+    console.log("CostoSalida =" + CostoSalida);
+   var saldoAnterior = parseInt((<HTMLInputElement>document.getElementById("Saldo"+(id-1))).value);
+   (<HTMLInputElement>document.getElementById("Haber"+id)).value = CostoSalida.toString();
+   (<HTMLInputElement>document.getElementById("Saldo"+id)).value = (saldoAnterior - CostoSalida).toString();
   }
 
   addFieldValue() {
@@ -53,5 +105,57 @@ export class FacpyaPrincipalComponent implements OnInit {
   deleteFieldValue(index:any) {
       this.fieldArray.splice(index, 1);
   }
+
+  HabilitarHabilitarEntrada(id:any)
+  {
+    var Entrada = (<HTMLInputElement>document.getElementById("Entrada"+id)).value;
+    if (Entrada != null || Entrada != undefined || Entrada != "")
+    {
+      console.log("sentrew");
+      (<HTMLInputElement>document.getElementById("Salida"+id)).disabled = true;
+      (<HTMLInputElement>document.getElementById("Haber"+id)).disabled = true;
+    }
+    if(Entrada == "")
+    {
+      console.log("entrew");
+      (<HTMLInputElement>document.getElementById("Salida"+id)).disabled = false;
+      (<HTMLInputElement>document.getElementById("Haber"+id)).disabled = false;
+    }
+  }
+  HabilitarHabilitarSalida(id:any)
+  {
+    var salida = (<HTMLInputElement>document.getElementById("Salida"+id)).value;
+    if (salida != null || salida != undefined || salida != ""  )
+    {
+      (<HTMLInputElement>document.getElementById("Entrada"+id)).disabled = true;
+      (<HTMLInputElement>document.getElementById("Debe"+id)).disabled = true;
+      (<HTMLInputElement>document.getElementById("Unitario"+id)).value =  (<HTMLInputElement>document.getElementById("Medio"+(id-1))).value ;
+
+    }
+    if(salida == "")
+    {
+      (<HTMLInputElement>document.getElementById("Entrada"+id)).disabled = false;
+      (<HTMLInputElement>document.getElementById("Debe"+id)).disabled = false;
+    }
+  }
+
+  calcularComprobacion()
+  {
+   var unidades:number = parseFloat((<HTMLInputElement>document.getElementById("udsexistenciacbp")).value);
+   var costomedio:number =  parseFloat((<HTMLInputElement>document.getElementById("costomediocbp")).value);
+    if(unidades == null || unidades == undefined)
+    {
+      unidades = 0;
+    }
+    if(costomedio == null || costomedio == undefined )
+    {
+      costomedio = 0;
+    }
+
+     (<HTMLInputElement>document.getElementById("resultadocbp")).value = (costomedio * unidades).toString()
+
+  }
+  
+
 
 }
